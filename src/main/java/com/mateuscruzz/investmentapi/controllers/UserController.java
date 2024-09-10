394c53168d7b0025376b440c5f822ca44aabc9ef
mateus <mateus.cruz14m@gmail.com>
2024-09-10 19:17:33 -0300

    create user method

--- src/main/java/com/mateuscruzz/investmentapi/controllers/UserController.java
+++ src/main/java/com/mateuscruzz/investmentapi/controllers/UserController.java
@@ -1,7 +1,26 @@
 package com.mateuscruzz.investmentapi.controllers;
 
+import com.mateuscruzz.investmentapi.model.User;
+import com.mateuscruzz.investmentapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDTO createUserDTO) {
+        var userId = userService.createUser(createUserDTO);
+        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString() )).build();
+    }
 }
