b77851dd140c017f2f744c19359b30032fd5079d
mateus <mateus.cruz14m@gmail.com>
2024-09-10 17:54:54 -0300

    created

---
+++ src/main/java/com/mateuscruzz/investmentapi/InvestmentapiApplication.java
@@ -0,0 +1,13 @@
+package com.mateuscruzz.investmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateuscruzz/investmentapi/InvestmentapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuscruzz.investmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
