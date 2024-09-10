ac0cef679461a5a87ce295c41a4111cb88786a95
mateus <mateus.cruz14m@gmail.com>
2024-09-10 18:33:54 -0300

    model User

---
+++ src/main/java/com/mateuscruzz/investmentapi/model/User.java
@@ -0,0 +1,35 @@
+package com.mateuscruzz.investmentapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updatedTimestamp;
+}
