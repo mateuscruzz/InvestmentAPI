7c4397bb52ff1e276196fb027918121c79c9d236
mateus <mateus.cruz14m@gmail.com>
2024-09-13 10:01:14 -0300

    model account

---
+++ src/main/java/com/mateuscruzz/investmentapi/model/Account.java
@@ -0,0 +1,24 @@
+package com.mateuscruzz.investmentapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_accounts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @Column(name = "account_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String description;
+}
